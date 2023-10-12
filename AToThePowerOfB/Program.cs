e36be61bd01a22613da03e722f36a0509163a1ce
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-10-13 01:08:56 +0300

    число A в целую степень B с помощью рекурсии

---
+++ AToThePowerOfB/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число A в целую степень B с помощью рекурсии
+int Pow(int a, int b)
+{
+  if (b == 0) return 1;
+  return a * Pow(a, b - 1);//A в степени B = A*A в степени(B-1)
+}
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Pow(a, b));
+
