d595b1648d6fbe430faec7b06b3877e1ffec2a44
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-10-13 00:51:56 +0300

    Рекурсия суммы цифр

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр
+// 453->12
+// 45->9
+int SumDigital(int number)
+{
+  if (number == 0)
+  {
+    return number;
+  }
+  return number % 10 + SumDigital(number / 10);
+}
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SumDigital(number));
